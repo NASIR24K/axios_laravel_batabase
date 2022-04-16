86c9cb3c720c670ba544f57fd15a6e4f5312f0b5
NASIR24K <45306542+NASIR24K@users.noreply.github.com>
2022-04-16 19:36:48 +0600

    Revert "custom.js, web.php, views/Insert/Update/Delete/Sel"
    This reverts commit 562a8aafe6fba3b1adb92d504f66a8abe24b2824.

--- views/DeleteData.blade.php
+++
@@ -1,5 +0,0 @@
-<input type="text" id="My_id" placeholder="Enter your ID"><br>
-<button onclick="DeleteOnData()">Delete</button>
-
-<script type="text/javascript" src={{asset('/js/axios.min.js')}}></script>
-<script type="text/javascript" src={{asset('/js/custom.js')}}></script>
